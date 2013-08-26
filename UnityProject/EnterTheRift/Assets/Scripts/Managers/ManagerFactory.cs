2562dfb474c12c8d1652ecdd9ae03c615435ddf6
Seurath <ante.bralic@gmail.com>
2013-08-26 18:59:07 -0400

    - Added ManagerFactory and UnitySceneUtil

--- UnityProject/EnterTheRift/Assets/Scripts/Managers/ManagerFactory.cs
+++ UnityProject/EnterTheRift/Assets/Scripts/Managers/ManagerFactory.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
 using System.Collections;
+using System.Reflection;
 
 public class ManagerFactory : MonoBehaviour
 {
+	[SerializeField] private InputManager inputManagerPrefab = null;
+	public static InputManager InputManager { get; private set; }
+	
+	[SerializeField] private SixenseInput sixenseInputPrefab = null;
+	public static SixenseInput SixenseInput { get; private set; }
+	
+	void Awake ()
+	{
+		GameObject.DontDestroyOnLoad(this.gameObject);
+		InitializeManagers();
+	}
+	
+	private void InitializeManagers ()
+	{
+		ManagerFactory.SixenseInput = InitializeManager<SixenseInput>(this.sixenseInputPrefab);
+		ManagerFactory.InputManager = InitializeManager<InputManager>(this.inputManagerPrefab);
+		ManagerFactory.InputManager.sixenseInputScript = ManagerFactory.SixenseInput;
+	}
+	
+	
+	#region Helpers
 
-	// Use this for initialization
-	void Start ()
+	private TManager InitializeManager<TManager> (UnityEngine.Object prefab) where TManager : MonoBehaviour
 	{
-	
+		GameObject instance = UnitySceneUtility.InstantiatePrefabAtTransform(prefab, this.transform);
+		GameObject.DontDestroyOnLoad(instance);
+		return instance.GetComponent<TManager>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
+
+	#endregion Helpers
 }
+
 
--- UnityProject/EnterTheRift/Assets/Scripts/Utilities/UnitySceneUtil.cs
+++ UnityProject/EnterTheRift/Assets/Scripts/Utilities/UnitySceneUtil.cs
@@ -1,12 +1,46 @@
 using System;
+using UnityEngine;
 
-namespace AssemblyCSharp
+public static class UnitySceneUtility
 {
-	public class UnitySceneUtil
+	public static GameObject AnchorGameObjectAtTransform (GameObject target, Transform transform, bool doNormalizeScale = true)
 	{
-		public UnitySceneUtil ()
+		target.transform.parent = transform;
+		target.transform.localPosition = Vector3.zero;
+		if (doNormalizeScale) {
+			target.transform.localScale = Vector3.one;
+		}
+		return target;
+	}
+	
+	public static GameObject InstantiatePrefabAtTransform (UnityEngine.Object prefab, Transform transform)
+	{
+		GameObject instance = GameObject.Instantiate(prefab) as GameObject;
+		if (instance == null)
+		{ 
+			Debug.LogError("Provided prefab object cannot be instantiated to a GameObject type.");
+		}
+		return AnchorGameObjectAtTransform(instance, transform);
+	}
+	
+	public static GameObject InstantiateGameObjectAtTransform (GameObject target, Transform transform)
+	{
+		GameObject instance = GameObject.Instantiate(target) as GameObject;
+		return AnchorGameObjectAtTransform(instance, transform);
+	}
+	
+	public static GameObject InstantiateGameObjectAtTransform (string name, Transform parentTransform)
+	{
+		GameObject gameObject = new GameObject(name);
+		return AnchorGameObjectAtTransform(gameObject, parentTransform);
+	}
+	
+	public static void DestroyChildren (Transform parentTransform)
+	{
+		for (int i = 0; i < parentTransform.GetChildCount(); i++) 
 		{
+			GameObject.Destroy(parentTransform.GetChild(i).gameObject);
 		}
 	}
+	
 }
-
